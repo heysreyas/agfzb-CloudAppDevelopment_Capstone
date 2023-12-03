b61871ce7e306303d73e74adddafa66ce919d9c3
heysreyas <heysreyas@gmail.com>
2023-12-03 19:42:39 +0530

    12 night

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -20,7 +20,7 @@ logger = logging.getLogger(__name__)
 def get_dealerships(request):
     if request.method == "GET":
         context = {}
-        url = "https://9bebcb01.eu-de.apigw.appdomain.cloud/api/dealership"
+        url = ""
         # Get dealers from the Cloudant DB
         context["dealerships"] = get_dealers_from_cf(url)
 
@@ -125,7 +125,7 @@ def get_dealerships(request):
 def get_dealer_details(request, dealer_id):
     context = {}
     if request.method == "GET":
-        url = 'https://9bebcb01.eu-de.apigw.appdomain.cloud/api/review'
+        url = ''
         reviews = get_dealer_reviews_from_cf(url, dealer_id=dealer_id)
         context = {
             "reviews":  reviews, 
@@ -142,7 +142,7 @@ def add_review(request, dealer_id):
     if request.user.is_authenticated:
         # GET request renders the page with the form for filling out a review
         if request.method == "GET":
-            url = f"https://5b93346d.us-south.apigw.appdomain.cloud/dealerships/dealer-get?dealerId={dealer_id}"
+            # url = f"={dealer_id}"
             # Get dealer details from the API
             context = {
                 "cars": CarModel.objects.all(),
@@ -171,7 +171,7 @@ def add_review(request, dealer_id):
             else: 
                 review["purchase_date"] = None
 
-            url = "https://9bebcb01.eu-de.apigw.appdomain.cloud/api/review"  # API Cloud Function route
+            # url = ""  # API Cloud Function route
             json_payload = {"review": review}  # Create a JSON payload that contains the review data
 
             # Performing a POST request with the review
