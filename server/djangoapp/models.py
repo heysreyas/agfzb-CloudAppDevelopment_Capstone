77a4bac06a2cde08c71c5bd6472b2d97781d67f1
heysreyas <heysreyas@gmail.com>
2023-12-03 13:14:10 +0530

    12 noon

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.timezone import now
+import datetime
 
 
 # Create your models here.
@@ -25,3 +26,66 @@ from django.utils.timezone import now
 
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+# Car Make model
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=50)
+    description = models.CharField(null=True, max_length=500)
+
+    def __str__(self):
+        return self.name
+
+
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, null=True, on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=50)
+    dealer_id = models.IntegerField(null=True)
+
+    SEDAN = "Sedan"
+    SUV = "SUV"
+    WAGON = "Wagon"
+    SPORT = "Sport"
+    COUPE = "Coupe"
+    MINIVAN = "Mini"
+    VAN = "Van"
+    PICKUP = "Pickup"
+    TRUCK = "Truck"
+    BIKE = "Bike"
+    SCOOTER = "Scooter"
+    OTHER = "Other"
+    CAR_CHOICES = [(SEDAN, "Sedan"), (SUV, "SUV"), (WAGON, "Station wagon"), (SPORT, "Sports Car"),
+                   (COUPE, "Coupe"), (MINIVAN, "Mini van"), (VAN,
+                                                             "Van"), (PICKUP, "Pick-up truck"),
+                   (TRUCK, "Truck"), (BIKE, "Motor bike"), (SCOOTER, "Scooter"), (OTHER, 'Other')]
+    model_type = models.CharField(
+        null=False, max_length=15, choices=CAR_CHOICES, default=SEDAN)
+
+    YEAR_CHOICES = []
+    for r in range(1969, (datetime.datetime.now().year+1)):
+        YEAR_CHOICES.append((r, r))
+
+    year = models.IntegerField(
+        ('year'), choices=YEAR_CHOICES, default=datetime.datetime.now().year)
+
+    def __str__(self):
+        return self.name + ", " + str(self.year) + ", " + self.model_type
+
+
+
+    # A plain Python class to hold dealer data
+class CarDealer:
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, state, zip):
+        self.address = address
+        self.city = city
+        self.full_name = full_name  # Full name of dealership
+        self.id = id  # Dealership id
+        self.lat = lat
+        self.long = long
+        self.short_name = short_name
+        self.st = st  # State alpha code
+        self.state = state  # Full state name
+        self.zip = zip
+        self.idx = 0
+
+    def __str__(self):
+        return self.full_name + ", " + self.state
+
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -13,19 +13,23 @@ urlpatterns = [
     # name the URL
 
     # path for about view
-
+    path(route='', view=views.get_dealerships, name='index'),
     # path for contact us view
 
     # path for registration
-
+    path(route='registration/', view=views.registration_request, name='registration'),
     # path for login
+    path(route='login/', view=views.login_request, name='login'),
 
     # path for logout
+    path(route='logout/', view=views.logout_request, name='logout'),
+    # path for logout
 
-    path(route='', view=views.get_dealerships, name='index'),
+
 
     # path for dealer reviews view
+    path(route='dealer/<int:dealer_id>/', view=views.get_dealer_details, name='dealer_details'),
 
     # path for add a review view
-
+    path(route='dealer/<int:dealer_id>/add-review/', view=views.add_review, name="add_review")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
